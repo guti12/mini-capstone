64ece68e1d5962ecef62d9cc73a46956dc3e6c06
“Jose <Gutierrez@Gutierrezs-MacBook-Air.local>
2017-05-07 16:59:49 -0500

    added new code

--- app/controllers/gelatos_controller.rb
+++ app/controllers/gelatos_controller.rb
@@ -1,6 +1,37 @@
 class GelatosController < ApplicationController
 	def gelato_items
-		@gelato = Gelato.first
+		@gelato = Gelato.all
 		render 'gelato_view.html.erb'
 	end
+
+	def index
+		@gelatos = Gelato.all
+	end
+
+	def show 
+		gelato_id = params[:id]
+		@gelato = Gelato.find_by(id: gelato_id)
+	end
+
+	def new
+		
+	end
+
+	def create
+		gelato = Gelato.new(flavor: params[:flavor],
+												price: params[:price],
+												description: params[:description]
+												)
+		gelato.save
+	end
+
 end
+
+# <h1>All Contacts</h1>
+# <% @contacts.each do |contact| %>
+#   <h2>First Name: <a href="www.google.com"><%= contact.first_name %></a></h2>
+#   <h2>Last Name: <%= contact.last_name %></h2>
+#   <h2><span>Email:</span> <%= contact.email %></h2>
+#   <h2>Phone: <%= contact.phone_number %></h2>
+#   <br>
+# <% end %>
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get '/gelato' => 'gelatos#gelato_items'
+
+  get '/' => 'gelatos#index'
+  get '/gelatos' => 'gelatos#index'
+
+  get '/gelatos/new' => 'gelatos#new'
+  post '/gelatos' => 'gelatos#create'
+
+  get '/gelatos/:id' => 'gelatos#show'
+
+  get 
+
 end
