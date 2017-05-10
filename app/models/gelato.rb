8d9a1ae99e0394055e82cbb0c59fcccb215e5ea2
“Jose <Gutierrez@Gutierrezs-MacBook-Air.local>
2017-05-09 21:40:46 -0500

    added new content

--- app/models/gelato.rb
+++ app/models/gelato.rb
@@ -1,2 +1,16 @@
 class Gelato < ApplicationRecord
+
+	def sale_message
+		if price.to_i <= 2
+			 "Discount item!" 
+		else
+			 "Everyday value!!" 
+		end
+	end
+
+	def tax
+		tax_price = (price.to_i + (price.to_i * 0.09))
+		tax_price
+	end
+
 end
