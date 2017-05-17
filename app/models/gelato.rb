ebc5cefeaa3787b4ad17714919076be15d9be9f0
“Jose <Gutierrez@Gutierrezs-MacBook-Air.local>
2017-05-16 21:46:11 -0500

    added new files

--- app/models/gelato.rb
+++ app/models/gelato.rb
@@ -1,5 +1,6 @@
 class Gelato < ApplicationRecord
 	belongs_to :supplier
+	has_many :images
 
 	def sale_message
 		if discounted?
@@ -13,6 +14,15 @@ class Gelato < ApplicationRecord
 		price < 3
 	end
 
+	def first_image_url
+		image_collection = images
+		if image_collection.length == 0
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png"
+		else
+			image_collection.first.first_image_url
+		end
+	end
+
 	def tax
 		price * 0.09
 	end
@@ -20,5 +30,7 @@ class Gelato < ApplicationRecord
 	def total
 		price + tax
 	end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516012039) do
+ActiveRecord::Schema.define(version: 20170517002031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170516012039) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "supplier_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "product_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "suppliers", force: :cascade do |t|
